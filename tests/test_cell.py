f03c323ea4cd884d37143991df1d186a86c888f0
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-01 16:47:36 +0200

    Fix

---
+++ tests/test_cell.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from TFMSTGCA.cell import *
+
+class TestCell(TestCase):
+    
+    def setUp(self):
+        TestCase.setUp(self)
+        position = (1,1,1)
+        self.genome = Genome(1,1,1,1,1)
+        self.cell = Cell(position, 1,1,1,1,1, 50, 10**5)
+
+    def test_get_genome_mutations(self):
+        self.assertEqual(self.genome.mutations(), 5)
+
+    def test_get_genome_mutations(self):
+        self.assertEqual(self.cell.mutations(), 5)
+
+    def test_decrease_telomer(self):
+        self.cell.decrease_telomer()
+        self.assertEqual(self.cell.tl, 49)
+
+if __name__ == '__main__':
+    unittest.main()
