22205480d69a13ccde6e592f7f1ad1b69b4921fa
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-01 16:52:18 +0200

    Fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TFMSTGCA',
-    version='0.1.1',
+    version='0.0.1',
     url='https://github.com/MULCIA/TFMSTGCA',
     license='MIT License',
     author='Sergio Rodriguez',
