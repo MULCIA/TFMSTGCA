2f4f61708f29a15ed10fd52ff102bc68a2d0a726
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-07-16 17:51:39 +0200

    Create test for atuomata with first test

---
+++ tests/test_automata.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from TFMSTGCA.automata import Automata
+from TFMSTGCA.simulation_globals import SimulationGlobals
+from TFMSTGCA.cell import Cell
+from TFMSTGCA.grid import Grid
+
+class TestAutomata(TestCase):
+
+    def setUp(self):
+        TestCase.setUp(self)
+        self.dimension = 3
+        self.iterations = 5000
+        self.simulationGlobals = SimulationGlobals(1,1,1,1,1,1,1,5,10)
+        self.automata = Automata(self.dimension, self.iterations, self.simulationGlobals)
+
+    def test_future_mitotic_event(self):
+        result = [self.automata.future_mitotic_event() for i in range(100)]
+        for res in result:
+            self.assertTrue(5 <= res <= 10)
