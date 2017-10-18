38582a49d1e910342bc562a57fb1f4c92ea41491
serrodcalº <sergiorodriguezcalvo@gmail.com>
2017-10-18 13:59:13 +0200

    Create plot functions

--- TFMSTGCA/analytics.py
+++ TFMSTGCA/analytics.py
@@ -4,61 +4,98 @@ from .cell import Cell
 
 class Analytics(object):
 
-    def __init__(self, cells, np_grid):
-        self.cells = cells
-        self.np_grid = np_grid
-
     def plot_cells(self, measure):
+        """
+        measure = {
+            'iterations': [1,2,3,4],
+            'cells': [10,350,352,435]
+            'healthy': [10,350,351,400],
+            'carcinogenic': [0,0,1,35]
+        }
+        """
+        plt.plot(measure['iterations'], measure['cells'])
+        plt.show()
+
+    def plot_health_vs_carcino(self, measure):
+        """
+        measure = {
+            'iterations': [1,2,3,4],
+            'cells': [10,350,352,435]
+            'healthy': [10,350,351,400],
+            'carcinogenic': [0,0,1,35]
+        }
+        """
+        plt.plot(measure['iterations'], measure['healthy'], '-', measure['iterations'], measure['carcinogenic'], 'bs')
+        plt.show()
+
+    def plot_mutations(self, measure):
+        """
+        measure = {
+            'iterations': [1,2,3,4],
+            'sg': [0,0,1,25],
+            'igi': [0,0,0,5],
+            'ea': [0,0,0,5],
+            'ei': [0,0,0,0],
+            'gi': [0,0,0,0]
+        }
+        """
+        plt.plot(
+                    measure['iterations'], measure['sg'], '-',
+                    measure['iterations'], measure['igi'], 'bs',
+                    measure['iterations'], measure['ea'], 'g^',
+                    measure['iterations'], measure['ei'], '-',
+                    measure['iterations'], measure['gi'], 'bs',
+                )
+        plt.show()
+
+    def plot_grid(self, np_grid):
         pass
 
-    def plot_grid(self):
-        pass
-
-    def sum_healthy_cells(self):
+    def sum_healthy_cells(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if str(cell) == '00000':
                 cont += 1
         return cont
 
-    def sum_carcinogenic_cells(self):
+    def sum_carcinogenic_cells(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if str(cell) != '00000':
                 cont += 1
         return cont
 
-    def sum_sg_mutations(self):
+    def sum_sg_mutations(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if cell.genome.sg:
                 cont += 1
         return cont
 
-    def sum_igi_mutations(self):
+    def sum_igi_mutations(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if cell.genome.igi:
                 cont += 1
         return cont
 
-    def sum_ea_mutations(self):
+    def sum_ea_mutations(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if cell.genome.ea:
                 cont += 1
         return cont
 
-    def sum_ei_mutations(self):
+    def sum_ei_mutations(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if cell.genome.ei:
                 cont += 1
         return cont
 
     def sum_gi_mutations(self, cells):
         cont = 0
-        for position,cell in self.cells.items():
+        for position,cell in cells.items():
             if cell.genome.gi:
                 cont += 1
         return cont
