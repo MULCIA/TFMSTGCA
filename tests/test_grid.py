784915186a93973a34d96e4d5647b76e7530d59a
serrodcalº <sergiorodriguezcalvo@gmail.com>
2017-10-20 11:18:21 +0200

    Add new tests for new methods in grid module

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -5,9 +5,6 @@ from TFMSTGCA.grid import Grid
 class TestExperiments(TestCase):
 
     def setUp(self):
-        TestCase.setUp(self)
-        position = (1,1,1)
-        first_cell = Cell(position, 1,1,1,1,1, 50, 10**5)
         self.grid = Grid(3, 3, 3)
 
     def test_neighborhood(self):
@@ -21,3 +18,12 @@ class TestExperiments(TestCase):
     def test_filter_True(self):
         result = self.grid.filter(-1, 10)
         self.assertEqual(result, False)
+
+    def test_build(self):
+        grid = self.grid.build()
+        self.assertEqual(grid[1][1][1], '')
+
+    def test_create_numpy_grid(self):
+        cells = {(1,1,1): Cell((1,1,1),0,0,0,0,0,50,10**5)}
+        grid = self.grid.create_numpy_grid(cells)
+        self.assertEqual(grid[1][1][1], '00000')
